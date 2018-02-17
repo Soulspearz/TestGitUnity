bb4d5fa04ce46de55b3df3fef475cc2b91723899
soulspearz <soulspearz12498@hotmail.com>
2018-02-17 11:14:37 +0700

    Halo

---
+++ Assets/Boxs.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boxs : MonoBehaviour {
+	float rotationx;
+	float rotationy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotationx += Time.deltaTime * 100;
+		rotationy += Time.deltaTime * 100;
+		transform.rotation = Quaternion.Euler (rotationx, rotationy, 0);
+
+		
+	}
+}
